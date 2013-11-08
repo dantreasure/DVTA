b2e9f10e0be4c49b04fa7f894d7fba510bdb0260
Daniel Treasure <dbtreasure@gmail.com>
2013-11-07 19:30:17 -0700

    adding angular, in the middle of adding angular-bootstrap

---
+++ public/javascripts/controllers/TasksController.js
@@ -0,0 +1,9 @@
+var TasksController = function ($scope){
+  $scope.selected = undefined;
+  $scope.tasks = [
+    {name: "Deadlift", count: 43},
+    {name: "Pages Read", count: 421},
+    {name: "Hours Coding", count: 34},
+    {name: "Romanian Deadlift", count: 32} 
+  ];
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'DVTA',
+    todos : [
+      {name: "Deadlift", count: 45},
+      {name: "Cups of water", count: 12},
+      {name: "Pages Read", count: 124}
+    ]
+  });
 };
